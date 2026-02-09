7070e6d7f7fae1ce4a1459eb0b01c2874f0be10b
praneethvelpumadugu-web <praneeth.velpumadugu@gmail.com>
2026-02-09 15:47:00 +0530

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main{
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("This is a simple Java program.");
+        System.out.println("It prints multiple lines of text to the console.");
     }
 }
